f0fbcb9a994792a60aab2b01813d2e018c357b33
msi <lg.mobile0808@gmail.com>
2025-06-17 13:33:11 +0300

    Solutionpytest

--- db/models.py
+++ db/models.py
@@ -1,9 +1,14 @@
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
+
+
+class User(AbstractUser):
+    pass
 
 
 class Genre(models.Model):
