dda991df11ffec728816bc2ab45eaaa40539bb28
msi <lg.mobile0808@gmail.com>
2025-06-17 12:58:06 +0300

    Solution

--- services/order.py
+++ services/order.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 @transaction.atomic
-def create_order(tickets: str, username: str, date: int=None) -> None:
+def create_order(tickets: str, username: str, date: int = None) -> None:
     user = User.objects.get(username=username)
     order = Order.objects.create(user=user)
     if date:
@@ -15,15 +15,15 @@ def create_order(tickets: str, username: str, date: int=None) -> None:
         order.save()
     for ticket in tickets:
         Ticket.objects.create(
-            order = order,
-            row = ticket["row"],
-            seat = ticket["seat"],
-            movie_session_id = ticket["movie_session"]
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"]
         )
     return order
 
 
-def get_orders(username: str=None) -> None:
+def get_orders(username: str = None) -> None:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
