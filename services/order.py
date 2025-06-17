68d5aefb382335902bd907a0fe186fc0f0402c3d
msi <lg.mobile0808@gmail.com>
2025-06-17 12:54:53 +0300

    Solution]

--- services/order.py
+++ services/order.py
@@ -15,15 +15,15 @@ def create_order(tickets: str, username: str, date: int=None) -> None:
         order.save()
     for ticket in tickets:
         Ticket.objects.create(
-            order=order,
-            row=ticket["row"],
-            seat=ticket["seat"],
-            movie_session_id=ticket["movie_session"]
+            order = order,
+            row = ticket["row"],
+            seat = ticket["seat"],
+            movie_session_id = ticket["movie_session"]
         )
     return order
 
 
 def get_orders(username: str=None) -> None:
     if username:
-        return Order.objects.filter(user__username = username)
+        return Order.objects.filter(user__username=username)
     return Order.objects.all()
